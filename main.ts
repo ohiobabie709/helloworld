3bb2ee3c193aa928bac2b1be3a8b292f0bdfaefa
ohiobabie709 <122560397+ohiobabie709@users.noreply.github.com>
2023-01-13 13:29:45 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,64 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
+    music.playMelody("C5 E B F E G C5 D ", 120)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . . . .
+        # # # . .
+        # . . # .
+        # . . # .
+        # . . # .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.SmallHeart)
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # # .
+        # . . # .
+        # # # . .
+        `)
 })
 basic.showIcon(IconNames.Silly)
