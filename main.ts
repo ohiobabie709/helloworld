fe1357df283c1504c62cde8da50fd24dc082c2ca
ohiobabie709 <122560397+ohiobabie709@users.noreply.github.com>
2023-01-13 13:15:17 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,4 +4,4 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.SmallHeart)
 })
-basic.showIcon(IconNames.House)
+basic.showIcon(IconNames.Silly)
